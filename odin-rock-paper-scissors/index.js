fb23b237a13b0450701759c3555dd93dade91b61
Alzter Aquino <43547673+alztr2908@users.noreply.github.com>
2023-08-21 22:23:13 +0800

    Finished index.js at odin-rock-paper-scissors & Add submodule
    CLI-based RPC in index.js for RPC game that has UI and submodule called
    javascript-exercise that is untracked.

--- odin-rock-paper-scissors/index.js
+++ odin-rock-paper-scissors/index.js
@@ -1,1 +1,34 @@
 console.log("Hello world!");
+
+function getComputerChoice() {
+  let choices = ["rock", "paper", "scissors"];
+  res = Math.floor(Math.random() * choices.length);
+
+  return choices[res];
+}
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+
+  if (playerSelection === computerSelection) {
+    return "It's a tie!";
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return `You win! ${playerSelection} beats ${computerSelection}`;
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return `You win! ${playerSelection} beats ${computerSelection}`;
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return `You win! ${playerSelection} beats ${computerSelection}`;
+  } else {
+    return `You lose! ${computerSelection} beats ${playerSelection}`;
+  }
+}
+
+function game() {
+  for (var i = 0; i < 5; i++) {
+    const playerSelection = prompt("Rock, Paper, Scissors?");
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+  }
+}
+
+game();
