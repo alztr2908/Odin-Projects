343cff097fac15d548ef17fa03afe5e76fb4c715
Alzter Aquino <43547673+alztr2908@users.noreply.github.com>
2023-08-26 05:10:54 +0800

    Divide the game logic to different functions

--- odin-rock-paper-scissors/index.js
+++ odin-rock-paper-scissors/index.js
@@ -24,56 +24,50 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-function game() {
-  for (var i = 0; i < 5; i++) {
-    const playerSelection = prompt("Rock, Paper, Scissors?");
-    const computerSelection = getComputerChoice();
-    console.log(playRound(playerSelection, computerSelection));
+function displayResults(results, playerSelection, computerSelection) {
+  if (results == "win") {
+    playerScore.textContent = parseInt(playerScore.textContent) + 1;
+    resultsDisplay.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
+  } else if (results == "lose") {
+    computerScore.textContent = parseInt(computerScore.textContent) + 1;
+    resultsDisplay.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
+  } else {
+    resultsDisplay.textContent = `It's a tie!`;
   }
 }
 
-// const rockButton = document.querySelector("#rock");
-// const paperButton = document.querySelector("#paper");
-// const scissorsButton = document.querySelector("#scissors");
+function setMatch(currPlayerScore, currComputerScore) {
+  if (currPlayerScore == 5) {
+    resultsDisplay.textContent += `You won the game! You win!`;
+    return;
+  }
+  if (currComputerScore == 5) {
+    resultsDisplay.textContent += `You lost the game! Computer wins!`;
+    return;
+  }
+}
+
 const choicesButton = document.querySelectorAll("button");
 const playerScore = document.querySelector(".playerScore");
 const computerScore = document.querySelector(".computerScore");
 const resultsDisplay = document.querySelector(".resultsDisplay");
-// var playerSelection = "";
 
-console.log(choicesButton);
-
-choicesButton.forEach((choice) => {
-  choice.addEventListener("click", () => {
-    playerSelection = choice.textContent;
-    const computerSelection = getComputerChoice();
-
+choicesButton.forEach((choiceButton) => {
+  choiceButton.addEventListener("click", () => {
     // Reset score once computer or player score reached to 5
     if (playerScore.textContent == 5 || computerScore.textContent == 5) {
       playerScore.textContent = "0";
       computerScore.textContent = "0";
     }
 
+    const playerSelection = choiceButton.textContent;
+    const computerSelection = getComputerChoice();
+    const result = playRound(playerSelection, computerSelection);
+
     // Score points and display match results
-    if (playRound(playerSelection, computerSelection) == "win") {
-      playerScore.textContent = parseInt(playerScore.textContent) + 1;
-      resultsDisplay.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
-    } else if (playRound(playerSelection, computerSelection) == "lose") {
-      computerScore.textContent = parseInt(computerScore.textContent) + 1;
-      resultsDisplay.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
-    } else {
-      resultsDisplay.textContent = `It's a tie!`;
-    }
+    displayResults(result, playerSelection, computerSelection);
 
     // Display winner when either score is 5
-    if (playerScore.textContent == 5 || computerScore.textContent == 5) {
-      if (playerScore.textContent == 5) {
-        resultsDisplay.textContent += `You won the game! You win!`;
-      } else if (computerScore.textContent == 5) {
-        resultsDisplay.textContent += `You lost the game! Computer wins!`;
-      }
-    }
+    setMatch(playerScore.textContent, computerScore.textContent);
   });
 });
-
-// game();
