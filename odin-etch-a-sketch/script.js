7eabaf57ba9ae9772abbeb741b40e6c739fb77e9
Alzter Aquino <43547673+alztr2908@users.noreply.github.com>
2023-08-27 15:52:30 +0800

    Initialize files | Add style on JS created element
    Use setAttribute to add the style from css to the created element of JS.
    Prereq of doing a grid on this project

---
+++ odin-etch-a-sketch/script.js
@@ -0,0 +1,18 @@
+/*
+
+for row  
+    div    
+    for column -> flex
+        div
+    
+*/
+
+// sanity check
+console.log("Hello World!");
+
+const word = document.createElement("h1");
+word.textContent = "Hello World!";
+
+document.body.appendChild(word);
+
+word.setAttribute("class", "word");
