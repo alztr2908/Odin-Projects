e6c04fc9827b35bb3568618f99895ea387a6c5d3
Alzter Aquino <43547673+alztr2908@users.noreply.github.com>
2023-08-28 02:52:46 +0800

    Make the cell size dynamic to grid size
    Use grid template instead of flex for dynamic sizing of the cells to the
    grid

--- odin-etch-a-sketch/script.js
+++ odin-etch-a-sketch/script.js
@@ -1,33 +1,28 @@
-/*
-
-for row  
-    div    
-    for column -> flex
-        div
-    
-*/
-
 function createGrid() {
   // Define the grid dimensions
-  const numRows = 16;
-  const numCols = 16;
+  const size = 10;
+
+  grid.style.gridTemplateColumns = `repeat(${size},1fr)`;
+  grid.style.gridTemplateRows = `repeat(${size},1fr)`;
 
   // Create the grid
-  for (let row = 0; row < numRows; row++) {
-    const rowContainer = document.createElement("div");
-    rowContainer.classList.add("grid-row");
-    for (let col = 0; col < numCols; col++) {
-      const cell = document.createElement("div");
-      cell.classList.add("grid-cell");
-      cell.addEventListener("mouseover", () => {
-        cell.style.backgroundColor = "black";
-        //   cell.setAttribute("class", "grid-cell-colored");
-        //   console.log(cell);
-      });
-      rowContainer.appendChild(cell);
-    }
-    grid.appendChild(rowContainer);
+  for (let i = 0; i < size * size; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("grid-cell");
+    cell.addEventListener("mouseover", () => {
+      cell.style.backgroundColor = "black";
+    });
+    grid.appendChild(cell);
   }
+}
+
+function clearGrid() {
+  grid.textContent = ""; // remove all child of the grid
+}
+
+function resetGrid() {
+  clearGrid();
+  createGrid(); // create new grid
 }
 
 // sanity check
@@ -38,7 +33,4 @@ const resetButton = document.querySelector(".resetButton");
 
 createGrid();
 
-resetButton.addEventListener("click", () => {
-  grid.textContent = ""; // remove all child of the grid
-  createGrid(); // create new grid
-});
+resetButton.addEventListener("click", resetGrid);
