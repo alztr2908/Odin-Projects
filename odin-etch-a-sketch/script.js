7b689bd367c3b21b8d70ff9803c0ecd6853e8769
Alzter Aquino <43547673+alztr2908@users.noreply.github.com>
2023-08-27 16:23:27 +0800

    Add initial grid (no events yet)

--- odin-etch-a-sketch/script.js
+++ odin-etch-a-sketch/script.js
@@ -10,9 +10,22 @@ for row
 // sanity check
 console.log("Hello World!");
 
-const word = document.createElement("h1");
-word.textContent = "Hello World!";
+const grid = document.querySelector(".grid");
+// const rowContainer =
 
-document.body.appendChild(word);
+// Define the grid dimensions
+const numRows = 5;
+const numCols = 5;
 
-word.setAttribute("class", "word");
+// Create the grid
+for (let row = 0; row < numRows; row++) {
+  const rowContainer = document.createElement("div");
+  rowContainer.classList.add("grid-row");
+  for (let col = 0; col < numCols; col++) {
+    const cell = document.createElement("div");
+    cell.classList.add("grid-cell");
+    // cell.textContent = `(${row}, ${col})`; // You can customize the cell content here
+    rowContainer.appendChild(cell);
+  }
+  grid.appendChild(rowContainer);
+}
