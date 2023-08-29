f0a6aae8ee764a17f91e33cdb6ff2ce858f5ad68
Alzter Aquino <43547673+alztr2908@users.noreply.github.com>
2023-08-30 03:10:59 +0800

    Add functionality on delete and clear button

--- odin-calculator/script.js
+++ odin-calculator/script.js
@@ -1,11 +1,43 @@
 // sanity check
 console.log("Hello world!");
 
+let firstOperand = "";
+let secondOperand = "";
+let operator = null;
+
 const keys = document.querySelectorAll(".keys");
-console.log(keys);
+const lastOperationScreen = document.querySelector("#lastOperationScreen");
+const currentOperationScreen = document.querySelector(
+  "#currentOperationScreen"
+);
+
+function deleteResults() {
+  currentOperationScreen.textContent = currentOperationScreen.textContent
+    .toString()
+    .slice(0, -1);
+}
+
+function resetResults() {
+  currentOperationScreen.textContent = "";
+  firstOperand = "";
+  secondOperand = "";
+  operator = null;
+}
+
+function updateCurrentOperationScreen(val) {
+  if (val == "DELETE") {
+    deleteResults();
+  } else if (val == "CLEAR") {
+    resetResults();
+  } else {
+    currentOperationScreen.textContent += val;
+  }
+}
+
+function updateValue(e) {
+  updateCurrentOperationScreen(e.target.innerText);
+}
 
 keys.forEach((key) => {
-  key.addEventListener("click", () => {
-    console.log(key.innerText);
-  });
+  key.addEventListener("click", updateValue);
 });
